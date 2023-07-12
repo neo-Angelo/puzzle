a2eab6af0e79c868b72b83d54d57e2453d00be45
neo-Angelo <dantehellsing41@gmail.com>
2023-07-12 18:59:11 -0300

    a

---
+++ PuzzleGame/Assets/MainScripts/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string nameItem;
+    public int id;
+    public Sprite icon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
--- PuzzleGame/Assets/MainScripts/pickUpItem.cs
+++ PuzzleGame/Assets/MainScripts/pickUpItem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class pickUpItem : MonoBehaviour
 {
     public GameObject ItemText;
+    public Item item;
        
        void Start()
     {
